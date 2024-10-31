075977dabcdd700de989edee8141f2fd1950494d
thatjar <64643552+thatjar@users.noreply.github.com>
2024-10-31 22:41:58 +0530

    update third party list

--- src/routes/third-parties/+page.svelte
+++ src/routes/third-parties/+page.svelte
@@ -5,31 +5,64 @@
 <h1>List of Third Parties</h1>
 <ul>
   <li>
-    <a href="https://pypi.org">PyPI</a> - for the "pypi" command which returns details
-    of a given package
+    <a href="https://pypi.org">PyPI</a> <br />
+    <b>Data sent:</b> package name (explicitly provided by user with the
+    <code>pypi</code> command)
   </li>
+  <br />
+
   <li>
-    <a href="https://www.npmjs.org">NPM</a> - for the "npm" command which returns
-    details of a given package
+    <a href="https://www.npmjs.org">NPM</a> <br />
+    <b>Data sent:</b> package name (explicitly provided by user with the
+    <code>npm</code> command)
   </li>
+  <br />
+
   <li>
-    <a href="https://github.com">GitHub</a> - for the "github" command which searches
-    a repository and returns its URL
+    <a href="https://github.com">GitHub</a> <br />
+    <b>Data sent:</b> search query (explicitly provided by user with the
+    <code>github</code> command)
   </li>
+  <br />
+
   <li><a href="https://genius.com">Genius</a> - original source of lyrics</li>
+  <br />
+
   <li>
-    <a href="https://some-random-api.com">Some Random Api</a> - for the "lyrics"
-    command which obtains lyrics from Genius
+    <a href="https://some-random-api.com">Some Random Api</a> - obtains lyrics
+    from Genius <br />
+    <b>Explicitly provided data sent with the commands:</b>
+    <ul>
+      <li><code>lyrics</code></li>
+      <li><code>megamind</code></li>
+    </ul>
+    <b>No data sent with the commands:</b>
+    <ul>
+      <li><code>dog</code></li>
+      <li><code>cat</code></li>
+      <li><code>panda</code></li>
+    </ul>
   </li>
+  <br />
+
   <li>
-    <a href="https://popcat.xyz/api">Pop Cat API</a> - for the commands "pickupline"
-    (takes no data), "weather", and "quote" (which take user provided data)
+    <a href="https://popcat.xyz/api">Pop Cat API</a> <br />
+    <b>Explicitly provided data sent with the commands:</b>
+    <ul>
+      <li><code>weather</code></li>
+    </ul>
+    <b>User avatar and text sent with the commands:</b>
+    <ul>
+      <li><code>quote</code></li>
+    </ul>
   </li>
+  <br />
+
   <li>
-    <a href="https://icanhazdadjoke.com">icanhazdadjoke</a> - for the "dadjoke" command.
-    Does not take any data.
+    <a href="https://icanhazdadjoke.com">icanhazdadjoke</a> - for the
+    <code>dadjoke</code> command. Does not take any data.
   </li>
-  <li>and Discord itself.</li>
+  <br />
 </ul>
 
 <div style="height: 50px;"></div>
